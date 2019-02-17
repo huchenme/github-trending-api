6545ea5664ab2fd671b4b4c0c8c03d282c129ed0
Wanchao Zhang <wonderbeyond@gmail.com>
2019-02-17 18:12:23 +0800

    fix a bug before response from cache

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ app.get('/repositories', async (req, res) => {
     const cacheKey = `repositories::${language || 'nolang'}::${since ||
       'daily'}`;
     const cached = cache.get(cacheKey);
-    if (Boolean(cached) && cache.length > 0) {
+    if (Boolean(cached) && cached.length > 0) {
       return res.json(cached);
     }
     const data = await fetchRepositories({ language, since });
@@ -57,7 +57,7 @@ app.get('/developers', async (req, res) => {
     const { language, since } = req.query;
     const cacheKey = `developers::${language || 'nolang'}::${since || 'daily'}`;
     const cached = cache.get(cacheKey);
-    if (Boolean(cached) && cache.length > 0) {
+    if (Boolean(cached) && cached.length > 0) {
       return res.json(cached);
     }
     const data = await fetchDevelopers({ language, since });
