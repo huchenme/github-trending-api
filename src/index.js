b8240c7bf2cd2f3bd7c53ffb01e244a30687ba67
Hu Chen <huchenme@users.noreply.github.com>
2019-04-19 14:17:39 +0800

    feat: Add `random` to JS API (#31)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import { sample, sampleSize } from 'lodash';
 
 const SERVER_URL = 'https://github-trending-api.now.sh';
 
@@ -13,16 +14,48 @@ function buildUrl(baseUrl, params = {}) {
 }
 
 export async function fetchAllLanguages() {
-  const data = await fetch(`${SERVER_URL}/languages`);
-  return data.json();
+  const res = await fetch(`${SERVER_URL}/languages`);
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error('Something went wrong');
+  }
 }
 
 export async function fetchRepositories(params) {
-  const data = await fetch(buildUrl(`${SERVER_URL}/repositories`, params));
-  return data.json();
+  const res = await fetch(buildUrl(`${SERVER_URL}/repositories`, params));
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error('Something went wrong');
+  }
 }
 
 export async function fetchDevelopers(params) {
-  const data = await fetch(buildUrl(`${SERVER_URL}/developers`, params));
-  return data.json();
+  const res = await fetch(buildUrl(`${SERVER_URL}/developers`, params));
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error('Something went wrong');
+  }
+}
+
+export async function fetchRandomRepository(params) {
+  const res = await fetch(buildUrl(`${SERVER_URL}/repositories`, params));
+  if (res.ok) {
+    const json = res.json();
+    return sample(json);
+  } else {
+    throw new Error('Something went wrong');
+  }
+}
+
+export async function fetchRandomRepositories(size = 1, params) {
+  const res = await fetch(buildUrl(`${SERVER_URL}/repositories`, params));
+  if (res.ok) {
+    const json = res.json();
+    return sampleSize(json, size);
+  } else {
+    throw new Error('Something went wrong');
+  }
 }
