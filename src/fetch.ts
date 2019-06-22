bab1866c9ed3f5629b8b1e5e3a8a51bce0a25477
Hu Chen <chen@huchen.me>
2019-06-22 14:21:34 +0800

    feat: add avatar for repositories

--- src/fetch.ts
+++ src/fetch.ts
@@ -68,86 +68,92 @@ export async function fetchRepositories({
     $('.Box article.Box-row')
       .get()
       // eslint-disable-next-line complexity
-      .map((repo): any => {
-        const $repo = $(repo);
-        const title = $repo
-          .find('.h3')
-          .text()
-          .trim();
-        const relativeUrl = $repo
-          .find('.h3')
-          .find('a')
-          .attr('href');
-        const currentPeriodStarsString =
-          $repo
-            .find('.float-sm-right')
+      .map(
+        (repo): any => {
+          const $repo = $(repo);
+          const title = $repo
+            .find('.h3')
             .text()
-            .trim() || /* istanbul ignore next */ '';
-
-        const builtBy = $repo
-          .find('span:contains("Built by")')
-          .find('[data-hovercard-type="user"]')
-          .map((i, user): any => {
-            const altString = $(user)
-              .children('img')
-              .attr('alt');
-            const avatarUrl = $(user)
-              .children('img')
-              .attr('src');
-            return {
-              username: altString
-                ? altString.slice(1)
-                : /* istanbul ignore next */ null,
-              href: `${GITHUB_URL}${user.attribs.href}`,
-              avatar: removeDefaultAvatarSize(avatarUrl),
-            };
-          })
-          .get();
-
-        const colorNode = $repo.find('.repo-language-color');
-        const langColor = colorNode.length
-          ? colorNode.css('background-color')
-          : null;
-
-        const langNode = $repo.find('[itemprop=programmingLanguage]');
-
-        const lang = langNode.length
-          ? langNode.text().trim()
-          : /* istanbul ignore next */ null;
-
-        return omitNil({
-          author: title.split(' / ')[0],
-          name: title.split(' / ')[1],
-          url: `${GITHUB_URL}${relativeUrl}`,
-          description:
+            .trim();
+          const [username, repoName] = title.split(' / ');
+          const relativeUrl = $repo
+            .find('.h3')
+            .find('a')
+            .attr('href');
+          const currentPeriodStarsString =
             $repo
-              .find('p.my-1')
+              .find('.float-sm-right')
               .text()
-              .trim() || /* istanbul ignore next */ '',
-          language: lang,
-          languageColor: langColor,
-          stars: parseInt(
-            $repo
-              .find(`[href="${relativeUrl}/stargazers"]`)
-              .text()
-              .replace(',', '') || /* istanbul ignore next */ '0',
-            10
-          ),
-          forks: parseInt(
-            $repo
-              .find(`[href="${relativeUrl}/network/members"]`)
-              .text()
-              .replace(',', '') || /* istanbul ignore next */ '0',
-            10
-          ),
-          currentPeriodStars: parseInt(
-            currentPeriodStarsString.split(' ')[0].replace(',', '') ||
-              /* istanbul ignore next */ '0',
-            10
-          ),
-          builtBy,
-        });
-      })
+              .trim() || /* istanbul ignore next */ '';
+
+          const builtBy = $repo
+            .find('span:contains("Built by")')
+            .find('[data-hovercard-type="user"]')
+            .map(
+              (i, user): any => {
+                const altString = $(user)
+                  .children('img')
+                  .attr('alt');
+                const avatarUrl = $(user)
+                  .children('img')
+                  .attr('src');
+                return {
+                  username: altString
+                    ? altString.slice(1)
+                    : /* istanbul ignore next */ null,
+                  href: `${GITHUB_URL}${user.attribs.href}`,
+                  avatar: removeDefaultAvatarSize(avatarUrl),
+                };
+              }
+            )
+            .get();
+
+          const colorNode = $repo.find('.repo-language-color');
+          const langColor = colorNode.length
+            ? colorNode.css('background-color')
+            : null;
+
+          const langNode = $repo.find('[itemprop=programmingLanguage]');
+
+          const lang = langNode.length
+            ? langNode.text().trim()
+            : /* istanbul ignore next */ null;
+
+          return omitNil({
+            author: username,
+            name: repoName,
+            avatar: `${GITHUB_URL}/${username}.png`,
+            url: `${GITHUB_URL}${relativeUrl}`,
+            description:
+              $repo
+                .find('p.my-1')
+                .text()
+                .trim() || /* istanbul ignore next */ '',
+            language: lang,
+            languageColor: langColor,
+            stars: parseInt(
+              $repo
+                .find(`[href="${relativeUrl}/stargazers"]`)
+                .text()
+                .replace(',', '') || /* istanbul ignore next */ '0',
+              10
+            ),
+            forks: parseInt(
+              $repo
+                .find(`[href="${relativeUrl}/network/members"]`)
+                .text()
+                .replace(',', '') || /* istanbul ignore next */ '0',
+              10
+            ),
+            currentPeriodStars: parseInt(
+              currentPeriodStarsString.split(' ')[0].replace(',', '') ||
+                /* istanbul ignore next */ '0',
+              10
+            ),
+            builtBy,
+          });
+        }
+      )
   );
 }
 
@@ -161,46 +167,48 @@ export async function fetchDevelopers({
   const $ = cheerio.load(await data.text());
   return $('.Box article.Box-row')
     .get()
-    .map((dev): any => {
-      const $dev = $(dev);
-      const relativeUrl = $dev.find('.h3 a').attr('href');
-      const name = getMatchString(
-        $dev
-          .find('.h3 a')
-          .text()
-          .trim(),
-        /^\((.+)\)$/i
-      );
-
-      const username = relativeUrl.slice(1);
-
-      const type = $dev
-        .find('img')
-        .parent()
-        .attr('data-hovercard-type');
-
-      const $repo = $dev.find('.mb-2');
-
-      $repo.find('svg').remove();
-
-      return omitNil({
-        username,
-        name,
-        type,
-        url: `${GITHUB_URL}${relativeUrl}`,
-        avatar: removeDefaultAvatarSize($dev.find('img').attr('src')),
-        repo: {
-          name: $repo
-            .find('a')
+    .map(
+      (dev): any => {
+        const $dev = $(dev);
+        const relativeUrl = $dev.find('.h3 a').attr('href');
+        const name = getMatchString(
+          $dev
+            .find('.h3 a')
             .text()
             .trim(),
-          description:
-            $repo
-              .find('.f6')
+          /^\((.+)\)$/i
+        );
+
+        const username = relativeUrl.slice(1);
+
+        const type = $dev
+          .find('img')
+          .parent()
+          .attr('data-hovercard-type');
+
+        const $repo = $dev.find('.mb-2');
+
+        $repo.find('svg').remove();
+
+        return omitNil({
+          username,
+          name,
+          type,
+          url: `${GITHUB_URL}${relativeUrl}`,
+          avatar: removeDefaultAvatarSize($dev.find('img').attr('src')),
+          repo: {
+            name: $repo
+              .find('a')
               .text()
-              .trim() || /* istanbul ignore next */ '',
-          url: `${GITHUB_URL}${$repo.find('a').attr('href')}`,
-        },
-      });
-    });
+              .trim(),
+            description:
+              $repo
+                .find('.f6')
+                .text()
+                .trim() || /* istanbul ignore next */ '',
+            url: `${GITHUB_URL}${$repo.find('a').attr('href')}`,
+          },
+        });
+      }
+    );
 }
