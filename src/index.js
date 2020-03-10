881d718f34c1ae37b6f0ecdc1795ff07009901b3
Hu Chen <chen@huchen.me>
2020-03-10 22:37:57 +0800

    feat: use axios for fetching

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
 import { sample, sampleSize, snakeCase } from 'lodash';
 import languages from './languages.json';
 import spokenLanguages from './spoken-languages.json';
@@ -15,36 +15,36 @@ function buildUrl(baseUrl, params = {}) {
 }
 
 export async function fetchRepositories(params) {
-  const res = await fetch(buildUrl(`${SERVER_URL}/repositories`, params));
-  if (!res.ok) {
+  const res = await axios(buildUrl(`${SERVER_URL}/repositories`, params));
+  if (res.status !== 200) {
     throw new Error('Something went wrong');
   }
-  return res.json();
+  return res.data;
 }
 
 export async function fetchDevelopers(params) {
-  const res = await fetch(buildUrl(`${SERVER_URL}/developers`, params));
-  if (!res.ok) {
+  const res = await axios(buildUrl(`${SERVER_URL}/developers`, params));
+  if (res.status !== 200) {
     throw new Error('Something went wrong');
   }
-  return res.json();
+  return res.data;
 }
 
 export async function fetchRandomRepository(params) {
-  const res = await fetch(buildUrl(`${SERVER_URL}/repositories`, params));
-  if (!res.ok) {
+  const res = await axios(buildUrl(`${SERVER_URL}/repositories`, params));
+  if (res.status !== 200) {
     throw new Error('Something went wrong');
   }
-  const json = res.json();
+  const json = res.data;
   return sample(json);
 }
 
 export async function fetchRandomRepositories(size = 1, params) {
-  const res = await fetch(buildUrl(`${SERVER_URL}/repositories`, params));
-  if (!res.ok) {
+  const res = await axios(buildUrl(`${SERVER_URL}/repositories`, params));
+  if (res.status !== 200) {
     throw new Error('Something went wrong');
   }
-  const json = res.json();
+  const json = res.data;
   return sampleSize(json, size);
 }
 
