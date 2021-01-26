5fa8d550dbc22ee82bdcba425723ec60f1be63b4
Jorge <66563811+jorgeAgoiz@users.noreply.github.com>
2021-01-26 15:45:37 +0100

    Update developers.js

--- src/functions/developers.js
+++ src/functions/developers.js
@@ -5,7 +5,7 @@ exports.handler = async ({ queryStringParameters: params }) => {
   try {
     const { language, since } = params;
     const data = await fetchDevelopers({ language, since });
-    (data && data.length > 0) ? success(data) : success([]);
+    (data && data.length > 0) ? success(data) : success([]); 
   } catch (err) {
     console.error(err);
     error(err);
