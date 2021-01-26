6618e5da911a15a9e93c01f75efdf3f0623e1f30
Jorge <66563811+jorgeAgoiz@users.noreply.github.com>
2021-01-26 15:47:02 +0100

    Update repositories.js

--- src/functions/repositories.js
+++ src/functions/repositories.js
@@ -5,11 +5,7 @@ exports.handler = async ({ queryStringParameters: params }) => {
   try {
     const { language, since, spoken_language_code: spokenLanguage } = params;
     const data = await fetchRepositories({ language, since, spokenLanguage });
-    if (data && data.length > 0) {
-      return success(data);
-    } else {
-      return success([]);
-    }
+    (data && data.length > 0) ? success(data) : success([]);
   } catch (err) {
     console.error(err);
     error(err);
